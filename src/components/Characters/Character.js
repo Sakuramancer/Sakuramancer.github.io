2322335fbf77ee869b40d79ad42370b9b70f1f87
Sakuramancer <sakuramancer+github@gmail.com>
2023-06-19 13:12:47 +0400

    add mobile-first responsive design

--- src/components/Characters/Character.js
+++ src/components/Characters/Character.js
@@ -1,17 +1,14 @@
-import Card from "../UI/Card";
 import classes from "./Character.module.css";
 
 const Character = (props) => {
   const { id, title, img, alt } = props;
 
   return (
-    <li className={classes.item}>
-      <Card>
-        <img className={classes.image} src={img} alt={alt} height="300" />
-        <div className={classes.title}>
-          {title}
-          </div>
-      </Card>
+    <li className={classes.card}>
+      <img className={classes.image} src={img} alt={alt} />
+      <div className={classes.title}>
+        {title}
+      </div>
     </li>
   );
 };
--- src/components/Layout/MainHeader.js
+++ src/components/Layout/MainHeader.js
@@ -3,7 +3,8 @@ import classes from './MainHeader.module.css';
 const MainHeader = (props) => {
   return (
     <header className={classes.header}>
-      <h1>Кампания из Эвенглена</h1>
+      <h1 className={classes.desktop}>Кампания из Эвенглена</h1>
+      <h1 className={classes.mobile}>КиЭ</h1>
       <nav>
         <ul>
         </ul>
