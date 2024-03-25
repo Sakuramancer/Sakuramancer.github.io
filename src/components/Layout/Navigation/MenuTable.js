93cb25ecc31ec1d208958791bedce0e14ddf29d7
Sakuramancer <sakuramancer+github@gmail.com>
2024-03-25 19:07:19 +0400

    move menu data into utils folder

--- src/components/Layout/Navigation/MenuTable.js
+++ src/components/Layout/Navigation/MenuTable.js
@@ -1,54 +1,9 @@
 import MenuLink from "./MenuLink";
+import { menuData } from "../../../utils/menu";
 
 import classes from "./MenuTable.module.css";
 import palletes from "../../../palletes.module.css";
 
-const menuData = [
-  {
-    type: "link",
-    visible: false,
-    to: "",
-    label: "Ссылки",
-  },
-  {
-    type: "link",
-    visible: true,
-    to: "/kie/characters",
-    label: "Персонажи",
-  },
-  {
-    type: "link",
-    visible: true,
-    to: "/kie/magicItems",
-    label: "Предметы",
-  },
-  {
-    type: "link",
-    visible: false,
-    to: "",
-    label: "Заклинания",
-  },
-  { type: "line" },
-  {
-    type: "link",
-    visible: false,
-    to: "/kie/initiatives",
-    label: "Инициативы",
-  },
-  { type: "line" },
-  {
-    type: "link",
-    visible: true,
-    to: "/kie/addComment",
-    label: "Оставить комментарий",
-  },
-  {
-    type: "link",
-    visible: false,
-    to: "",
-    label: "Настройки",
-  },
-];
 const MenuTable = ({ schemeId, onConfirm }) => {
   const pallete = palletes[schemeId];
   return (
---
+++ src/utils/menu.js
@@ -0,0 +1,52 @@
+export const menuData = [
+  {
+    type: "link",
+    visible: false,
+    to: "",
+    label: "Ссылки",
+  },
+  {
+    type: "link",
+    visible: true,
+    to: "/kie/characters",
+    label: "Персонажи",
+  },
+  {
+    type: "link",
+    visible: true,
+    to: "/kie/magicItems",
+    label: "Предметы",
+  },
+  {
+    type: "link",
+    visible: true,
+    to: "/kie/sessions",
+    label: "Сессии",
+  },
+  {
+    type: "link",
+    visible: false,
+    to: "",
+    label: "Заклинания",
+  },
+  { type: "line" },
+  {
+    type: "link",
+    visible: false,
+    to: "/kie/initiatives",
+    label: "Инициативы",
+  },
+  { type: "line" },
+  {
+    type: "link",
+    visible: true,
+    to: "/kie/addComment",
+    label: "Оставить комментарий",
+  },
+  {
+    type: "link",
+    visible: false,
+    to: "",
+    label: "Настройки",
+  },
+];
